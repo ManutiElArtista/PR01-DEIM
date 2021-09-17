a0c34cd085da05aea32f4be54484abdf8202e28c
Guillermo Manuel Jiménez Núñez <guillermomjn@gmail.com>
2021-09-17 17:22:54 +0200

    Ejercicio01
    Ejercicio 01 realizado.

---
+++ Ejercicios PR01_MiniPracticas/Assets/Scripts/Ejercicio01.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio01 : MonoBehaviour
+{
+    public int num = 10;
+    private int num1 = 30;
+    public float dec = 19f;
+    public bool file = false;
+    public string word = "Hola";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(num);
+        print(dec);
+        print(file);
+        print(word);
+        print(num + num1);
+        print(num - num1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
